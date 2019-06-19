cd185af57aaad2209e807776de2f3906c1e8a476
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-06-19 11:24:24 +0200

    Add twig-bundle and improving routes

--- src/Controller/CountRedirectController.php
+++ src/Controller/CountRedirectController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use App\Entity\WatchedLink;
 use App\Entity\Visit;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
