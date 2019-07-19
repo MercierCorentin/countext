4a529c57ceb73b9203c5eaf21ffc711299dfa98e
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-07-19 12:49:32 +0200

    Add search feature on titles with exact string

--- public/js/searchBar.js
+++ public/js/searchBar.js
@@ -2,15 +2,26 @@ $(function(){
     $(".search-bar").on('input', function(){
         
         // declare and init vars
-        var searchValue = $(this).val().toLowerCase();
+        var searchValue = $(this).val()
         var titles = [];
+
+        // Search value transformation
+        searchValue = searchValue.toLowerCase();
+        searchValue = searchValue.trim();
 
         // Get all titles in JQuery elements
         var JQueryTitles = $("article h3");
 
         // Get all titles text
         JQueryTitles.each( function(){
-            titles.push($(this).html().toLowerCase()); 
+            
+            var currentValue = $(this).html();
+            currentValue = currentValue.toLowerCase();
+
+            if( currentValue === searchValue){
+                JQueryTitles.parent().hide();
+                $(this).parent().show();
+            }
         });
     })
 });
